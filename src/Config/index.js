c560b6793766ca12f28a43778881c7087d9ab4dc
Kaleab <kaleabyalewdeg46@gmail.com>
2020-04-04 00:34:12 +0300

    first commit

---
+++ src/Config/index.js
@@ -0,0 +1,7 @@
+export default {
+    endpoint: 'https://stephchatbot.core.ushaflow.io', // <- enter your gateway URL here
+    muted: true, // <- mute microphone at start
+    start_suggestions: ["Hello"], // <- array of suggestions, displayed at the start screen
+    fallback_lang: 'en', // <- fallback language code, if history mode or network is unavailable
+    voice: 'native' // <- voice of Text-To-Speech; for reference, see: https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisVoice/voiceURI
+}
